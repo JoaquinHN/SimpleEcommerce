a41792b6e8ce74d7bacf31a3c266414c6b246b1f
Danny Joaquin Rosa Cantarero <dannycantarero3@gmail.com>
2020-09-27 05:40:07 -0600

    Primer commit

--- bckseCommerce/routes/index.js
+++ bckseCommerce/routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+router.get("/info", function (req, res) {
+  var infoObj = {};
+  infoObj.cuenta = "1007199900439";
+  infoObj.nombre = "Danny Joaquin Rosa Cantarero";
+  infoObj.email = "dannycantarero2@gmail.com";
+  res.json(infoObj);
+});
+
+router.get("/hook", function (req, res) {
+  res.render("hook", {});
+});
+
+router.post("/hook", function (req, res) {
+  var body = req.body;
+  console.log(body);
+  res.render("hook", body);
+});
 module.exports = router;
